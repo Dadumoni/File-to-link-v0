aada7be51d03bb4bf9fd7eb2444c4d6e84b3653c
Dadumoni <165320151+Dadumoni@users.noreply.github.com>
2025-01-13 15:36:44 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,20 +9,20 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '219124813'))
-    API_HASH = str(getenv('API_HASH', 'c578b64ac7af5hak363f9e0ebfbc67923'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', '5881909650:AAGTWflaXg3SDjSIhhksk5GVvxtan1onXGs'))
+    API_ID = int(getenv('API_ID', '28589543'))
+    API_HASH = str(getenv('API_HASH', '59e9b183efecd1b03f2a2193224c0cae'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '7924433485:AAEAAxUHJ7YhO8NpXRv3LS87X-Nl6Yv-sj4'))
     name = str(getenv('name', 'Fast_Downloader_bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001977350650'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002440250342'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5313004751 866072531").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "7797542425").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Rk_botowner'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Bad_Karma_26'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -35,9 +35,9 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://mongodbuser:mogodbpass@cluster0.appjt7j.mongodb.net/'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Rk_botz'))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split()))      
-    SHORTLINK_URL = getenv('SHORTLINK_URL', 'onepagelink.in')
-    SHORTLINK_API = getenv('SHORTLINK_API', 'c47e1c4469c0a66e74af7153cb8f4d3b304d010')
-    TUTORIAL_URL = getenv('TUTORIAL_URL', 'https://t.me/rk_back_up/18')
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://dadumoni05:5HV7kXM2EdrjcMWG@cluster0.3wcnpfx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', ''))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))      
+    SHORTLINK_URL = getenv('SHORTLINK_URL', 'tglinkz.in')
+    SHORTLINK_API = getenv('SHORTLINK_API', '774da2406e0d06d12f69c6b6d79ff47a45c23701')
+    TUTORIAL_URL = getenv('TUTORIAL_URL', '')
